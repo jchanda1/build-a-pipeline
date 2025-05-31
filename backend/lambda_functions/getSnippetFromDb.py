6193e8ba82bde74e7ea7f6f34bbde098c4ef2de8
jchanda1 <joyanto.chanda@gmail.com>
2025-05-31 19:34:40 +0100

    add example lambda function that accesses db

---
+++ backend/lambda_functions/getSnippetFromDb.py
@@ -0,0 +1,36 @@
+import json 
+import boto3
+from decimal import Decimal
+
+client = boto3.client('dynamodb')
+dynamodb = boto3.resource("dynamodb")
+tableName = 'b-a-p-1612-gitlab'
+table = dynamodb.Table(tableName)
+
+def lambda_handler(event, context):
+    # TODO implement
+    print(event['routeKey'])
+    body = {}
+    statusCode = 200
+    try:
+        if event['routeKey'] == "GET /example_bap_dynamo":
+            res = table.get_item(
+                Key = {
+                    "stage":"install",
+                    "optionals":"cache"
+                }
+            )
+            print(res)
+            body = res
+            statusCode = 200
+    except KeyError:
+        statusCode = 400
+        body = 'Unsupported route: ' + event['routeKey']
+    body = json.dumps(body)
+    return {
+        'statusCode': statusCode,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        'body': body
+    }
